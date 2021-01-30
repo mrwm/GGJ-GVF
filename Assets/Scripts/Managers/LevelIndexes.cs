95d7b295549789a10d8dc2d0b6ae373c1abc6148
william <mrwm@wksp2>
2021-01-30 04:44:00 -0800

    rename old level functions to scene functions and add level function
    planning to use GameObject.FindGameObjectWithTag and tag all the flags for level

---
+++ Assets/Scripts/Managers/LevelIndexes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum LevelIndexes
+{
+   LVONE   = 0,
+   LVTWO   = 1,
+   LVTHREE = 2,
+   LVFOUR  = 3,
+   LVFIVE  = 4
+}
