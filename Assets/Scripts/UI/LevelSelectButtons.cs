8aaaaf54226f268d92bd996ed937922b63f3bcbf
Mebin Skaria <mebins@gmail.com>
2021-01-29 21:05:18 -0800

    Main Menu x Level Select Linked

---
+++ Assets/Scripts/UI/LevelSelectButtons.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectButtons : MonoBehaviour
+{
+    public Button back, level1, level2, level3, level4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        back.onClick.AddListener(backOnClick);
+        level1.onClick.AddListener(leveloneOnClick);
+        level2.onClick.AddListener(leveltwoOnClick);
+        level3.onClick.AddListener(levelthreeOnClick);
+        level4.onClick.AddListener(levelfourOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void backOnClick()
+    {
+        SceneManager.LoadScene("Main Menu");
+        
+    }
+    void leveloneOnClick()
+    {
+
+    }
+    void leveltwoOnClick()
+    {
+
+    }
+    void levelthreeOnClick()
+    {
+
+    }
+    void levelfourOnClick()
+    {
+
+    }
+}
---
+++ Assets/Scripts/UI/MainMenuButtons.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public Button btn_levels, btn_start;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn_start.onClick.AddListener(StartOnClick);
+        btn_levels.onClick.AddListener(LevelsOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void StartOnClick()
+    {
+        Debug.Log("Start Button Pressed");
+    }
+    void LevelsOnClick()
+    {
+        SceneManager.LoadScene("Level Select");
+    }
+}
