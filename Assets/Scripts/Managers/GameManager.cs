25497d56586b781022d8c051f063adcb17516c7f
Mebin Skaria <mebins@gmail.com>
2021-01-30 08:58:33 -0800

    Revert "Merge branch 'master' into LoadingScreen"
    This reverts commit c08cf0f0518318ac0f7a73acb2014540e1529585, reversing
    changes made to ea9a10d1ca3cd124953bfeefe62a4864388134aa.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -6,30 +6,22 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-
     private SceneIndexes current_level_index;
     private static GameManager instance;
-LoadingScreen
     public bool second_load = true;
 
     public float health_level = 1.0f;
     public int number_of_chests_collected = 0;
     public int total_gacha_currency = 0;
     public int total_princess_saved = 0;
-    private static int[] unlockedLevels = {1,0,0,0};
-    private SceneIndexes current_scene_index;
-    private LevelIndexes current_level_index;
-    private static GameManager instance; 
-
 
     // Start is called before the first frame update
     private void Start()
     {
         instance = this;
         DontDestroyOnLoad(this.gameObject);
-        current_scene_index = SceneIndexes.MAINMENU;
-        current_level_index = LevelIndexes.LVONE;
-        instance.setScene(current_scene_index);
+        current_level_index = SceneIndexes.MAINMENU;
+        instance.setLevel(current_level_index);
         LoadGame();
     }
     public static GameManager getManager()
@@ -37,30 +29,9 @@ LoadingScreen
         //singleton pattern
         return instance;
     }
-
-    public static int[] getUnlockedLevels()
-    {
-        return unlockedLevels;
-    }
-    // TODO unlocking level 2 would be unlockLevel(2)
-    // public void unlockLevel(levelNumber){
-    //     int index = levelNumber - 1;
-    //     unlockedLevels[levelNumber - 1] = 1;
-    // }
     public void setLevel (SceneIndexes level)
-
-    public void setScene (SceneIndexes level)
     {
-        current_scene_index = level;
-    }
-    public int getScene()
-    {
-        return (int)current_scene_index;
-    }
-
-    public void setLevel (LevelIndexes lvl)
-    {
-        current_level_index = lvl;
+        current_level_index = level;
     }
     public int getLevel()
     {
@@ -73,9 +44,7 @@ LoadingScreen
     }
     public void LoadGame()
     {
-
         instance.second_load = !instance.second_load;
-        SceneManager.LoadScene((int)current_scene_index);
-
+        SceneManager.LoadScene((int)current_level_index);
     }
 }
--- Assets/Scripts/UI/LevelSelectButtons.cs
+++ Assets/Scripts/UI/LevelSelectButtons.cs
@@ -8,56 +8,20 @@ public class LevelSelectButtons : MonoBehaviour
 {
     public Button back, level1, level2, level3, level4;
     private GameManager gm;
+
     // Start is called before the first frame update
     void Start()
     {
-        initializeLevels();
         back.onClick.AddListener(backOnClick);
-        updateLevels(GameManager.getUnlockedLevels());
+        level1.onClick.AddListener(leveloneOnClick);
+        level2.onClick.AddListener(leveltwoOnClick);
+        level3.onClick.AddListener(levelthreeOnClick);
+        level4.onClick.AddListener(levelfourOnClick);
         gm = GameManager.getManager();
     }
     private void Awake()
     {
         
-    }
-
-    public void initializeLevels(){
-        level1.interactable = false;
-        level2.interactable = false;
-        level3.interactable = false;
-        level4.interactable = false;
-    }
-
-    public void updateLevels(int[] levels){
-        
-       for (int i = 0; i < levels.Length -1; i++)
-        {
-           if(levels[i] == 1) {
-             activateLevel(i+1);
-           }
-        }
-    }
-
-    public void activateLevel(int number){
-      switch (number) 
-      {
-       case 1:
-        level1.interactable = true;
-        level1.onClick.AddListener(leveloneOnClick);
-        break;
-       case 2:
-        level2.interactable = true;
-        level2.onClick.AddListener(leveloneOnClick);
-        break;
-        case 3:
-        level3.interactable = true;
-        level3.onClick.AddListener(leveloneOnClick);
-        break;
-       case 4:
-        level4.interactable = true;
-        level4.onClick.AddListener(leveloneOnClick);
-        break;
-      } 
     }
 
     // Update is called once per frame
@@ -73,27 +37,20 @@ public class LevelSelectButtons : MonoBehaviour
     }
     void leveloneOnClick()
     {
-        gm.setScene(SceneIndexes.GAME);
-        gm.setLevel(LevelIndexes.LVONE);
+        gm.setLevel(SceneIndexes.GAME);
         SceneManager.LoadScene((int)SceneIndexes.LOADING);
         
     }
     void leveltwoOnClick()
     {
-        gm.setScene(SceneIndexes.GAME);
-        gm.setLevel(LevelIndexes.LVTWO);
 
     }
     void levelthreeOnClick()
     {
-        gm.setScene(SceneIndexes.GAME);
-        gm.setLevel(LevelIndexes.LVTHREE);
 
     }
     void levelfourOnClick()
     {
-        gm.setScene(SceneIndexes.GAME);
-        gm.setLevel(LevelIndexes.LVFOUR);
 
     }
 }
