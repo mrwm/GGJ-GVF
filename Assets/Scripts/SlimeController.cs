2ea3fd755d6366248a7a739644b6ee90232dffa4
william <mrwm@wksp2>
2021-01-31 05:11:08 -0800

    add main character sprite + slime properties

--- Assets/Scripts/SlimeController.cs
+++ Assets/Scripts/SlimeController.cs
@@ -7,10 +7,23 @@ public class SlimeController : MonoBehaviour{
   private float player_health;
   private float slime_health;
 
+  public float leftBoundLimit;
+  public float rightBoundLimit;
+  public bool moving;
+  public bool jumping;
+  public float jumpDelay = 3f;
+  public float jumpForce = 3f;
+  private float speed = 1f;
+
+  private bool isGrounded = true;
+  private Rigidbody2D rigidBody;
+
   // Start is called before the first frame update
   void Start(){
     gm = GameManager.getManager();
     slime_health = 5f;
+
+    rigidBody = GetComponent<Rigidbody2D>();
 
     InvokeRepeating("animateSlime", 2f, 1f);
   }
@@ -18,8 +31,16 @@ public class SlimeController : MonoBehaviour{
   // Update is called once per frame
   void Update(){
     if (slime_health < 0)
-      //delete self
       Destroy(gameObject);
+    if (moving)
+      moveHorizontal();
+    if (jumping){
+      if(isGrounded){
+        jumpTime();
+        isGrounded = false;
+        StartCoroutine(onTheGround());
+      }
+    }
   }
 
   void OnTriggerEnter2D(Collider2D other){
@@ -41,5 +62,30 @@ public class SlimeController : MonoBehaviour{
 
     //transform.localScale = Vector3.Lerp(transform.localScale, slime_scale, 10f * Time.deltaTime);;
     transform.localScale = slime_scale;
+  }
+
+  void moveHorizontal(){
+    Vector3 pos = transform.position;
+    pos.x += speed * Time.deltaTime;
+    transform.position = pos;
+    // Changing Direction
+    if ( pos.x < leftBoundLimit ){
+      speed = Mathf.Abs(speed); // Move right
+      transform.localScale = new Vector2(transform.localScale.x,transform.localScale.y);
+    }
+    else if ( pos.x > rightBoundLimit ){
+      speed = -Mathf.Abs(speed); // Move left
+      transform.localScale = new Vector2(-transform.localScale.x,transform.localScale.y);
+      Debug.Log(leftBoundLimit);
+    }
+  }
+
+  void jumpTime(){
+    rigidBody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+  }
+
+  public IEnumerator onTheGround(){
+    yield return new WaitForSeconds(jumpDelay);
+    isGrounded = true;
   }
 }
