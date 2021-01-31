1363a257bfb6d5fc075cd7470aa900270a6dbafa
Dipro Chowdhury <achowdh1@mail.sfsu.edu>
2021-01-31 02:08:57 -0800

    footsteps and jumping sound effects added

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -14,6 +14,8 @@ public class PlayerControler : MonoBehaviour{
   private bool isTouchGround;
   private Animator playerAnimation;
   private AudioSource audioSrc;
+  public AudioClip footsteps;
+  public AudioClip jump;
   private Vector2 spriteScale;
   private bool doubleJump;
 
@@ -34,18 +36,17 @@ public class PlayerControler : MonoBehaviour{
     // Player movement Logic
     if (movement != 0f){
       if(!audioSrc.isPlaying){
-        audioSrc.Play();
+        audioSrc.PlayOneShot(footsteps);
       }
       rigidBody.velocity = new Vector2(movement*speed, rigidBody.velocity.y);
         if (movement > 0f)
           transform.localScale = new Vector2(spriteScale.x,spriteScale.y);
         else
           transform.localScale = new Vector2(-spriteScale.x,spriteScale.y);
-    }else{
-        audioSrc.Stop();
-      }
+    }
 
     if(Input.GetButtonDown("Jump")){
+      audioSrc.PlayOneShot(jump);
       if (doubleJump && !isTouchGround){
         rigidBody.velocity = new Vector2(rigidBody.velocity.x,jumpSpeed);
         doubleJump = false;
