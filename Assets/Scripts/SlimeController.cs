db028c80e41bc97325e91ec625c16f7ead546996
william <mrwm@wksp2>
2021-01-31 03:27:06 -0800

    +slimes

--- Assets/Scripts/SlimeController.cs
+++ Assets/Scripts/SlimeController.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SlimeController : MonoBehaviour{
+  private GameManager gm;
+  private float player_health;
+  private float slime_health;
 
   // Start is called before the first frame update
   void Start(){
-    
+    gm = GameManager.getManager();
+    slime_health = 5f;
   }
 
   // Update is called once per frame
@@ -15,6 +19,10 @@ public class SlimeController : MonoBehaviour{
   }
 
   void OnTriggerEnter2D(Collider2D other){
-    Debug.Log("triggered");
+    if (other.tag == "Player")
+      player_health = gm.getHealth();
+      Debug.Log("HIT");
+      player_health -= 1;
+      gm.setHealth(player_health);
   }
 }
